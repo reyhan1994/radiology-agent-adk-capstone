f107161f14a0a9f51a1d94fe23549448689a6b4a
reiahn.alinia <43085897+reyhan1994@users.noreply.github.com>
2025-11-30 10:53:40 +0330

    Update step.py

--- adk/step.py
+++ adk/step.py
@@ -1,1 +1,7 @@
-
+# src/adk/step.py
+class Step:
+    def __init__(self, name, agent, input_key, output_key):
+        self.name = name
+        self.agent = agent
+        self.input_key = input_key
+        self.output_key = output_key
